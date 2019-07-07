4c9507fe11acce05c34057c3af43552fe3fd6181
CallMeSaltFish <573519596@qq.com>
2019-07-07 11:49:00 +0800

    audio control
    ran

---
+++ GiveMe/Assets/AudioManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    private AudioSource audioSource;
+    private AudioClip[] bgms;
+    private void Awake()
+    {
+        DontDestroyOnLoad(this.gameObject);
+        GameObject[] temp = GameObject.FindGameObjectsWithTag("AudioSource");
+        if(temp.Length > 1)
+        {
+            Destroy(temp[1]);
+        }
+        audioSource = GetComponent<AudioSource>();
+        bgms = Resources.LoadAll<AudioClip>("Audios");
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
